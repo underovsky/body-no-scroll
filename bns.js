871db6a631270985512a0ac78a3b255802557c5f
Krzysztof Rusnarczyk <office@underovsky.com>
2015-12-13 01:15:26 +0100

    Project files

---
+++ bns.js
@@ -0,0 +1,43 @@
+/*
+Body No Scroll
+https://github.com/underovsky/body-no-scroll
+The MIT License (MIT)
+Copyright (c) 2015 Krzysztof Rusnarczyk
+*/
+(function(name) {
+	function BNS() {
+		var settings = {
+			prevScroll: 0,
+			prevPosition: '',
+			prevOverflow: '',
+			on: false
+		};
+		
+		var getPrev = function() {
+			settings.prevScroll = (window.pageYOffset || document.documentElement.scrollTop)  - (document.documentElement.clientTop || 0);
+			settings.prevPosition = document.body.style.position;
+			settings.prevOverflow = document.body.style.overflow;
+		};
+		
+		return {
+			on: function() {
+				if (settings.on) return;
+				settings.on = true;
+				getPrev();
+				document.body.style.position = 'fixed';
+				document.body.style.overflow = 'hidden';
+				document.body.style.top = -settings.prevScroll + 'px';
+			},
+			off: function() {
+				if (!settings.on) return;
+				settings.on = false;
+				document.body.style.position = settings.prevPosition;
+				document.body.style.overflow = settings.prevOverflow;
+				document.body.style.top = 0;
+				window.scrollTo(0, settings.prevScroll);
+			}
+		};
+	}
+	
+	window[name] = new BNS();
+})('BNS');
